acac79ef990542b084aa117465433cc7dec73350
Michael-Jia <michaelpiao2008@163.com>
2024-01-31 17:06:15 +0800

    Site updated: 2024-01-31 17:06:15

--- anzhiyu/random.js
+++
@@ -1,3 +0,0 @@
-var posts=["2023/12/06/My-First-Content/","2023/12/06/Second-Blog/","2023/12/04/hello-world/"];function toRandomPost(){
-    pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
-  };
